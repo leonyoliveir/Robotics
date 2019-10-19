602ad10c9e680d6cebeb18bf9dcee0facfdef19d
leonyoliveir <leony.oliveira.bernardo@gmail.com>
2019-10-19 11:29:03 -0300

    "Atividade 01"

--- atividade_01/topic_publisher.py
+++ atividade_01/topic_publisher.py
@@ -1,1 +1,14 @@
-import ros
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import Int32
+
+rospy.init_node('topic_publisher')
+pub = rospy.Publisher('counter', Int32)
+rate = rospy.Rate(2)
+count = 0
+
+while not rospy.is_shutdown():
+	pub.publish(count)
+ 	count += 1
+ 	rate.sleep()
